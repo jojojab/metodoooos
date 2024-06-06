9a689a23df408c8863a23747f5f0194212afe3c8
jojojab <jojojablon@gmail.com>
2024-06-06 00:17:09 -0300

    nuevo comit

--- ej2_2_resolucion.py
+++ ej2_2_resolucion.py
@@ -7,7 +7,7 @@ def plot_reconstruction(a_d, cant_images, d):
     for i in range(cant_images):
         row = i // 5
         col = i % 5
-        axs[row, col].imshow(a_d[i], cmap='gray')
+        axs[row, col].imshow(a_d[i].reshape(28, 28), cmap='gray')
         axs[row, col].axis('off')
     #     delete the empty axs
     fig.delaxes(axs[3, 4])
@@ -15,16 +15,33 @@ def plot_reconstruction(a_d, cant_images, d):
     plt.show()
     plt.close()
 
+def plot_reconstruction_all_dim(all_image_vectors, cant_images, dim):
+    fig, axs = plt.subplots(cant_images, len(dim))
+    for i in range(cant_images):
+        for j in range(len(dim)):
+            row = i
+            col = j
+            u_d, s_d, vt_d, a_d = image_reconstructed(all_image_vectors, dim[j])
+            axs[row, col].imshow(a_d[i].reshape(28, 28), cmap='gray')
+            axs[row, col].axis('off')
+            if i == 0:
+                axs[row, col].set_title(f'Dim = {dim[j]}')
+    fig.suptitle('Reconstruccion de imagenes para distintas dimensiones')
+    plt.show()
+    plt.close()
 
 def main():
     cant_images = 19
+    cant_images1 = 4
     images_directory = 'images1'
-    dim = [2, 3, 4, 5, 8, 10, 15, 17, 19]
+    dim = [15, 10, 5, 3, 2]
 
     all_image_vectors = images_to_matrix(images_directory, cant_images)
-    for d in dim:
-        u_d, s_d, vt_d, a_d = image_reconstructed(all_image_vectors, d)
-        plot_reconstruction(a_d.reshape(cant_images, 28, 28), cant_images, d)
+    plot_reconstruction_all_dim(all_image_vectors, cant_images1, dim)
+    u_d, s_d, vt_d, a_d = image_reconstructed(all_image_vectors, 2)
+    plot_reconstruction(a_d, cant_images, 2)
+    u_d, s_d, vt_d, a_d = image_reconstructed(all_image_vectors, 19)
+    plot_reconstruction(a_d, cant_images, 19)
 
 
 if __name__ == '__main__':
